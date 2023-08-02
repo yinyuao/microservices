f6383bd6f17f95f436310a5398a77300942dfbe0
yinyuao <yinyuao@kingsoft.com>
2023-08-02 14:15:28 +0800

    fix:服务客户端获取时间修改为北京时间

--- client/src/main/java/com/ksyun/start/camp/service/TimeServiceImpl.java
+++ client/src/main/java/com/ksyun/start/camp/service/TimeServiceImpl.java
@@ -12,8 +12,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * 代表远端时间服务接口实现
@@ -33,7 +37,7 @@ public class TimeServiceImpl implements TimeService {
         String services = restTemplate.getForObject("http://localhost:8180/api/discovery?name=time-service", String.class);
         List<ServiceInfo> service = jacksonMapper.fromJson(services, new TypeReference<List<ServiceInfo>>() {});
         if (service == null || service.isEmpty()) {
-            return null;
+            throw new RuntimeException("服务未注册！");
         }
 
         // Step 2: 调用远程服务
@@ -44,10 +48,31 @@ public class TimeServiceImpl implements TimeService {
 
         ApiResponse response = restTemplate.getForObject(builder.toUriString(), ApiResponse.class);
         if (response == null || response.getData() == null) {
-            return null;
+            throw new RuntimeException("获取时间失败！");
         }
-        System.out.println(response.getData());
         ResDataInfo timeDate = objectMapper.convertValue(response.getData(), ResDataInfo.class);
-        return timeDate.getResult();
+        String res = "";
+        try {
+            res = convertGMTToBeijingTime(timeDate.getResult());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    // 将GMT时间字符串转换为北京时间字符串
+    // gmtTimeStr：GMT时间字符串，格式为"yyyy-MM-dd HH:mm:ss"
+    // 返回：转换后的北京时间字符串
+    public String convertGMTToBeijingTime(String gmtTimeStr) throws ParseException {
+        SimpleDateFormat gmtFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        gmtFormatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        SimpleDateFormat beijingFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        beijingFormatter.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+
+        Date gmtTime = gmtFormatter.parse(gmtTimeStr);
+        String beijingTimeStr = beijingFormatter.format(gmtTime);
+
+        return beijingTimeStr;
     }
 }
